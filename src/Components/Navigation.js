cd6e11987f503cc4e2b4633c6575a9aac51fa0ee
Fidel <fidel.mh3@gmail.com>
2019-06-15 10:48:43 +0200

    Navigation component added

---
+++ src/Components/Navigation.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'React';
+
+class Navigation extends Component
+{
+    render()
+    {
+        return(
+            <nav className="navbar navbar-dark bg-dark">
+                <a href="" className="text-white ">
+                    Tasks
+                </a>
+            </nav>
+        )
+    }
+}
+
+export default Navigation;
