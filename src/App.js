5ace738b2b2e9ef7e66adf137fe42a0ad21d754e
Fidel <fidel.mh3@gmail.com>
2019-06-15 13:10:03 +0200

    Added cards

--- src/App.js
+++ src/App.js
@@ -1,14 +1,57 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-      <img src={logo} className="App-logo" alt="logo" />
-    </div>
-  );
+import { todos } from './todos.json'
+
+class App extends Component{
+  constructor()
+  {
+    super();
+    this.state =
+    {
+      todos
+    }
+  }
+  render() {
+    const todos = this.state.todos.map((todo, i) =>{
+      return(
+        <div className="col-md-3">
+          <div className="card mt-4">
+          <div className="card-header">
+            <h3>{ todo.title }</h3>
+            <span className="badge badge-pill badge-danger ml-2">
+              {todo.priority}
+            </span>
+          </div>
+          <div className="card-body">
+            <p> {todo.description} </p>
+            <p><mark>{todo.responsable}</mark>  </p>
+          </div>
+        </div>
+        </div>
+      )
+    })
+    return (
+      <div className="App">
+        <nav className="navbar navbar-dark bg-dark">
+          <a href="" className="text-white ">
+            Tasks
+            <span className="badge badge-pill badge-light ml-2">
+              {this.state.todos.length}
+            </span>
+          </a>
+        </nav>
+        <div className="container">
+          <div className="row mt-4">
+            { todos} 
+          </div>
+        </div>
+            
+        <img src={logo} className="App-logo" alt="logo" />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/TodoForm.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+
+class TodoForm extends Component
+{
+    constructor()
+  {
+    super();
+  }
+  render()
+  {
+      return (
+          <div className="card">
+            <form className="card-body">
+                <div className="form-group">
+                    <input 
+                        type="text"
+                        name="title"
+                        className="form-control"
+                        placeholder="Title"
+                    />
+                </div>
+                <div className="form-group">
+                    <input 
+                        type="text"
+                        name="responsible"
+                        className="form-control"
+                        placeholder="Responsible"
+                    />
+                </div>
+                <div className="form-group">
+                    <input 
+                        type="text"
+                        name="description"
+                        className="form-control"
+                        placeholder="Description"
+                    />
+                </div>
+            </form>
+          </div>
+      )
+  }
+}
+
+export default TodoForm;
